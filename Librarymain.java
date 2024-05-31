594cb3ac639a06b0fef258f8d7efec601a63ce99
Arini Fatihah <128214992+arinifthh@users.noreply.github.com>
2024-05-31 12:06:34 +0800

    Update Librarymain.java

--- Librarymain.java
+++ Librarymain.java
@@ -1,12 +1,16 @@
+//nanti ada 3 option, are you admin/member/new member, if new member they will fill details then admin need to accept them, to ensure library safety
 public class LibraryMain {
     public static void main(String[] args) throws Exception {
         
         Book b1=new Book("Kacau","Ariff Adly","Available");
-        BookInventory bi = new BookInventory(b1);
+        Member m1=new Member("M001","Mul","012345678");
+        Admin a1=new Admin("A012","Nur");
+        
+        BookManage bi = new BookManage(b1);
 
         bi.addBook(b1);
-
-        
+        a1.manageMember(m1);
     }
 }
+
 //still on updating
