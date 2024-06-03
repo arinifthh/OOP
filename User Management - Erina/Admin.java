62e2b6acaeb39cbf96d6ba5c43ff0cd3235da352
Arini Fatihah <128214992+arinifthh@users.noreply.github.com>
2024-06-03 21:22:33 +0800

    Update Admin.java

--- User Management - Erina/Admin.java
+++ User Management - Erina/Admin.java
@@ -1,33 +1,34 @@
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
+import java.util.List;
 
 public class Admin {
-    private String adminID;
-    private String adminName;
-    private ArrayList<Member> memberList;
+    private String name;
+    private String adminid;
+    private List<Book> managedBooks;
 
-    public Admin(String ID,String n) {
-        this.adminID=ID;
-        this.adminName=n;
-        memberList=new ArrayList<>();
+    public Admin(String n,String id) {
+        this.name = n;
+        this.adminid=id;
+        this.managedBooks = new ArrayList<>();
     }
 
-    public void manageMember(Member member) {
-        String choice;
-        choice=JOptionPane.showInputDialog("Choose Operation\n1. Accept Request New Member\n2. Reject Request new Member");
-        if(choice.equals("1"));
-        if (choice.equals("1")) {
-            int confirm = JOptionPane.showConfirmDialog(null, "Are you sure to accept this new member?", choice, JOptionPane.YES_NO_OPTION);
-            if (confirm == JOptionPane.YES_OPTION) {
-                memberList.add(member);
-                JOptionPane.showMessageDialog(null, "New member accepted successfully!");
-            }
-        }
-        else if (choice.equals("2")) {
-            JOptionPane.showMessageDialog(null, "New member request rejected.");
-        } else {
-            JOptionPane.showMessageDialog(null, "Invalid choice. Please enter 1 or 2.");
-        }        
+    public String getName() {
+        return name;
+    }
+
+    public String getAdminId() {
+        return adminid;
+    }
+
+    public void addBook(Book book) {
+        managedBooks.add(book);
+    }
+
+    public void removeBook(Book book) {
+        managedBooks.remove(book);
+    }
+
+    public List<Book> getManagedBooks() {
+        return managedBooks;
     }
 }
-
